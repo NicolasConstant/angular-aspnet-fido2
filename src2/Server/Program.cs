639ea982645af20af2b595e0b3e7a342ed4f860c
Nicolas Constant <github@nicolas-constant.com>
2024-12-05 21:34:27 -0500

    migrate API

--- src2/Server/Program.cs
+++ src2/Server/Program.cs
@@ -1,27 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+
 namespace Server
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    var builder = WebApplication.CreateBuilder(args);
+
+        //    // Add services to the container.
+
+        //    builder.Services.AddControllers();
+
+        //    var app = builder.Build();
+
+        //    // Configure the HTTP request pipeline.
+
+        //    app.UseHttpsRedirection();
+
+        //    app.UseAuthorization();
+
+
+        //    app.MapControllers();
+
+        //    app.Run();
+        //}
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            CreateHostBuilder(args).Build().Run();
+        }
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
         }
     }
 }
