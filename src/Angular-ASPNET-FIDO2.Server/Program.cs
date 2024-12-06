cc9c22c08668b2dfa691faed9778e494ca06bd1c
Nicolas Constant <github@nicolas-constant.com>
2024-12-05 19:33:28 -0500

    migrating

--- src/Angular-ASPNET-FIDO2.Server/Program.cs
+++ src/Angular-ASPNET-FIDO2.Server/Program.cs
@@ -2,31 +2,45 @@ namespace Angular_ASPNET_FIDO2.Server
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    var builder = WebApplication.CreateBuilder(args);
+
+        //    // Add services to the container.
+
+        //    builder.Services.AddControllers();
+
+        //    var app = builder.Build();
+
+        //    app.UseDefaultFiles();
+        //    app.UseStaticFiles();
+
+        //    // Configure the HTTP request pipeline.
+
+        //    app.UseHttpsRedirection();
+
+        //    app.UseAuthorization();
+
+
+        //    app.MapControllers();
+
+        //    app.MapFallbackToFile("/index.html");
+
+        //    app.Run();
+        //}
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            CreateHostBuilder(args).Build().Run();
+        }
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.MapFallbackToFile("/index.html");
-
-            app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
         }
     }
 }
--- src/angular-aspnet-fido2.client/src/proxy.conf.js
+++ src/angular-aspnet-fido2.client/src/proxy.conf.js
@@ -7,6 +7,7 @@ const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
+      "/assertionOptions"
     ],
     target,
     secure: false
